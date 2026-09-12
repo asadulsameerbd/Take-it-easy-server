289b8304b06ab70bd9eacce4dab1210eb25e39de
asadulsameerbd <asadulsameerbd>
2026-09-12 13:54:14 +0600

    fix cors

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    origin:
+      process.env.CLIENT_ORIGIN ||
+      "https://take-it-easy-client-ol45.vercel.app",
   }),
 );
 
